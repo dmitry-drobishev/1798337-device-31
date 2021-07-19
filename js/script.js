2df58733314129d50f5bffec9abafdc1de747b28
stooped-dog <mitya.drobishev.77rus@yandex.ru>
2021-07-19 16:40:16 +0300

    Оживляем интерактивные элементы(на 2 проверку)

--- js/script.js
+++ js/script.js
@@ -32,11 +32,15 @@ feedbackLink.addEventListener("click", function (evt) {
 feedbackClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.remove("modal-show");
+  feedbackPopup.classList.remove("modal-error");
 });
 
 feedbackForm.addEventListener("submit", function (evt) {
   if (!feedbackLogin.value || !feedbackMail.value || !feedbackMessage.value) {
     evt.preventDefault();
+    feedbackPopup.classList.remove("modal-error");
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+    feedbackPopup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
         localStorage.setItem("name", feedbackLogin.value);
@@ -49,6 +53,7 @@ window.addEventListener("keydown", function (evt) {
     if (feedbackPopup.classList.contains("modal-show")) {
       evt.preventDefault();
       feedbackPopup.classList.remove("modal-show");
+      feedbackPopup.classList.remove("modal-error");
     }
   }
 });
